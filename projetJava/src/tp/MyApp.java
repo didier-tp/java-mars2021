bc136c39c81abf221f6f621bb637afa8008a7f93
didier <didier@d-defrance.fr>
2021-03-08 14:27:19 +0100

    s

--- projetJava/src/tp/MyApp.java
+++ projetJava/src/tp/MyApp.java
@@ -4,7 +4,19 @@ public class MyApp {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world 2");
-
+        testType();
+	}
+	
+	
+	public static void testType() {
+		String chX="3.5";
+		double x = Double.parseDouble(chX);
+		double y = x -1;
+		System.out.println("y="+y);
+		int a=0; //-1 , 0 , 23 , (null impossible)
+		Integer b=null; // -1 , 0 , 23 , null (valeur inconnue ou non renseignée)
+		b = a; // ou bien = new Integer(a); en vieux java
+		System.out.println("b="+b);
 	}
 
 }
